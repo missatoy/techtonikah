27f62f12c0ceb6648ad7969760de63754417db4f
Missato Yamashita <rmissato@gmail.com>
2022-12-13 18:46:19 +0000

    added typing effect to the landing page with links

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,7 +1,16 @@
+import { NavLink } from "react-router-dom"
+
 export default function Home() {
   return (
     <div className="banner">
       <h1 className="techtonikah">TECHTONIKAH</h1>
+      <div className="menu-items typewriter">
+        <h2>
+          <NavLink className="menu-item" to="/prints">Prints,</NavLink>
+          <NavLink className="menu-item" to="/tattoos"> Tattoos,</NavLink>
+          <NavLink className="menu-item" to="/about"> About me</NavLink>
+        </h2>
+      </div>
     </div>
   )
 }
