4f749b28d7c64145465d2c3644c912ee8fcf7bfc
Missato Yamashita <rmissato@gmail.com>
2022-12-14 15:34:47 +0000

    refactored css

--- src/components/Prints.jsx
+++ src/components/Prints.jsx
@@ -8,6 +8,10 @@ export default function Prints() {
       <div className="container padding-top">
         <h2>Here's your prints</h2>
         <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam impedit soluta quia doloremque? Officia soluta, recusandae perspiciatis minus provident in! Repellat quia ipsum cumque est laboriosam, eaque quo aliquid excepturi!</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Sed omnis voluptatem deleniti. Tempore blanditiis, eveniet atque quia molestiae non enim praesentium alias ratione quo rem fugiat distinctio sed inventore et.</p>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Assumenda enim, reprehenderit asperiores tempora, excepturi autem atque fugit pariatur earum ipsum repudiandae fuga in rerum perspiciatis laborum non soluta velit sequi!</p>
+        <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Maiores expedita voluptate, optio placeat laboriosam corporis cumque error natus. Nihil nemo similique voluptatum magnam consequatur obcaecati architecto ratione ducimus impedit voluptatibus?</p>
+        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Facilis eius aliquid quam nemo dolore veniam illo nulla, veritatis beatae ipsam architecto eligendi, a aliquam numquam maiores voluptatem pariatur, ea eos?</p>
       </div>
       <Navbar />
     </>
