7aded973cf05d07b5a16e0a4643b1b752d96d31d
Missato Yamashita <rmissato@gmail.com>
2022-12-21 18:24:36 +0000

    home page done

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -3,9 +3,13 @@ import { NavLink } from "react-router-dom"
 export default function Home() {
   return (
     <div className="banner">
-      <h1 className="techtonikah">TECHTONIKAH</h1>
-      <div className="menu-items typewriter">
-        <h2><NavLink className="menu-item" to="/prints">Prints</NavLink>, <NavLink className="menu-item" to="/tattoos">Tattoos</NavLink>, <NavLink className="menu-item" to="/about">About me</NavLink></h2>
+      <div className="wide-screen">
+        <div className="text-boxes">
+          <h1 className="techtonikah">TECHTONIKAH</h1>
+          <div className="menu-items typewriter">
+            <h2><NavLink className="menu-item" to="/prints">Prints</NavLink>, <NavLink className="menu-item" to="/tattoos">Tattoos</NavLink>, <NavLink className="menu-item" to="/about">About me</NavLink></h2>
+          </div>
+        </div>
       </div>
     </div>
   )
