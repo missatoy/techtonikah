37779666287a88168d195b5de8fc3ef5aeefe321
Missato Yamashita <rmissato@gmail.com>
2022-12-21 15:56:25 +0000

    fixed hover from links homepage

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -5,11 +5,7 @@ export default function Home() {
     <div className="banner">
       <h1 className="techtonikah">TECHTONIKAH</h1>
       <div className="menu-items typewriter">
-        <h2>
-          <NavLink className="menu-item" to="/prints">Prints,</NavLink>
-          <NavLink className="menu-item" to="/tattoos"> Tattoos,</NavLink>
-          <NavLink className="menu-item" to="/about"> About me</NavLink>
-        </h2>
+        <h2><NavLink className="menu-item" to="/prints">Prints</NavLink>, <NavLink className="menu-item" to="/tattoos">Tattoos</NavLink>, <NavLink className="menu-item" to="/about">About me</NavLink></h2>
       </div>
     </div>
   )
