9efd92677ca685116563a1799a373110beacb94b
Missato Yamashita <rmissato@gmail.com>
2022-12-22 18:18:51 +0000

    styled page 404

--- src/components/Page404.jsx
+++ src/components/Page404.jsx
@@ -1,7 +1,15 @@
+import Navbar from "./Navbar"
+import Topbar from "./Topbar"
+
 export default function Page404() {
   return (
-    <div className="d-flex justify-content-center align-items-center position-absolute w-100 h-100">
-      <h2>This page doesn't exist, sorry</h2>
+    <>
+    <Topbar />
+    <div className="d-flex flex-column justify-content-center align-items-center position-absolute w-100 h-75">
+      <h2>404</h2>
+      <h2>Page not found</h2>
     </div>
+    <Navbar />
+    </>
   )
 }
