1a089d4f870beaa3f1268841f4fb59bfbe2ad9f0
Missato Yamashita <rmissato@gmail.com>
2023-01-16 11:48:32 +0000

    finished tattoos page

--- src/components/Tattoos.jsx
+++ src/components/Tattoos.jsx
@@ -1,15 +1,19 @@
 import Navbar from "./Navbars/Bottombar"
 import Topbar from "./Navbars/Topbar"
 import FormTattoos from "./Forms/FormTattoos"
+import RetroWindow from "./Visuals/RetroWindow"
 
 export default function Tattoos() {
+  const tattoosText = (
+    <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Minima tempora enim, pariatur accusamus doloribus qui possimus debitis sed voluptas nulla dolor aliquam saepe corporis voluptate voluptatem ipsum magni necessitatibus quia!</p>
+  )
+
   return (
     <>
       <Topbar />
-      <div className="container">
-        <h2>Here's your tattoos</h2>
-        <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. Magni velit quo fugit atque dignissimos non ullam iste corporis nostrum dolorem, ipsum aperiam dolor illum fugiat officiis illo, voluptatem voluptates pariatur?</p>
-        <FormTattoos />
+      <div className="container windows-layout mb-4">
+        <RetroWindow name="Tattoos" content={tattoosText} />
+        <RetroWindow name="Contact us" content={<FormTattoos />} />
       </div>
       <Navbar />
     </>
