cecd12f0fac51a4592378418fdef5cd8ab54ce4f
Missato Yamashita <rmissato@gmail.com>
2023-02-02 18:08:17 +0000

    built the loading bar on refs page

--- src/components/Refs.jsx
+++ src/components/Refs.jsx
@@ -1,13 +1,15 @@
 import Topbar from "./Navbars/Topbar.jsx"
 import Botbar from "./navbars/Botbar.jsx"
+import LoadingBar from "./visuals/LoadingBar.jsx"
 
 export default function Refs() {
 
   return (
     <>
       <Topbar />
-      <div className="container">
-
+      <div className="d-flex justify-content-center align-items-center flex-column about-banner">
+        <h1>Loading...</h1>
+        <LoadingBar />
       </div>
       <Botbar />
     </>
---
+++ src/components/Visuals/LoadingBar.jsx
@@ -0,0 +1,18 @@
+import {useEffect} from "react"
+
+export default function LoadingBar() {
+
+  useEffect(() => {
+    const progressBar = document.getElementsByClassName('progress-bar')[0]
+    setInterval(() => {
+      const computedStyle = getComputedStyle(progressBar)
+      const width = parseFloat(computedStyle.getPropertyValue('--width')) || 0
+      progressBar.style.setProperty('--width', width + .1)
+    }, 5);
+  })
+
+
+  return (
+    <div className="progress-bar" style={{'--width': 0}} data-label=""></div>
+  )
+}
