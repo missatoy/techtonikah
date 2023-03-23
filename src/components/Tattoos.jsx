406a600c033f956774f9e787e5c55117e42c832d
Missato Yamashita <rmissato@gmail.com>
2023-03-23 13:49:39 +0000

    capitalized all tattoo text

--- src/components/Tattoos.jsx
+++ src/components/Tattoos.jsx
@@ -6,18 +6,18 @@ import Botbar from "./navbars/Botbar.jsx";
 export default function Tattoos() {
   const tattoosText = (
     <p>
-      handpoke (rendpoque, poc poc, poke, tatuagem sem máquina,...) é uma técnica de pigmentação corporal manual, ou seja, sem eletricidade. somente a agulha e a tinta na pele :) <br />
+      Handpoke (rendpoque, poc poc, poke, tatuagem sem máquina,...) é uma técnica de pigmentação corporal manual, ou seja, sem eletricidade. somente a agulha e a tinta na pele :) <br />
       <br />
-      práticas de modificação corporal como pigmentação, perfuração ou escarnificação são muito antigas e tradicionais, a maneira de realizar esses procedimentos é construída culturalmente e se renova constantemente por quem a pratica, mas o ínicio da história da tatuagem começa na pigmentação manual, exemplos que sustentam sua tradição até hoje são: o tebori no Japão ou o batok nas Filipinas para o povo Kalinga e na Nova Zelândia , em contextos cerimoniais, ou seja, de importância social e espiritual. práticas muito voltadas ao processo todo, para além do estético. e que carregam suas tecnologias de biossegurança correspondentes aos seus tempos históricos. <br />
+      Práticas de modificação corporal como pigmentação, perfuração ou escarnificação são muito antigas e tradicionais, a maneira de realizar esses procedimentos é construída culturalmente e se renova constantemente por quem a pratica, mas o ínicio da história da tatuagem começa na pigmentação manual, exemplos que sustentam sua tradição até hoje são: o tebori no Japão ou o batok nas Filipinas para o povo Kalinga e na Nova Zelândia, em contextos cerimoniais, ou seja, de importância social e espiritual. Práticas muito voltadas ao processo todo, para além do estético, e que carregam suas tecnologias de biossegurança correspondentes aos seus tempos históricos. <br />
       <br />
-      com o tempo e contato de diferentes culturas essas práticas tomaram novas formas, a máquina de tatuagem (elétricas ou pens) surgem pra facilitar o processo, e demarcam outras expressões culturais. <br />
+      Com o tempo e contato de diferentes culturas essas práticas tomaram novas formas, a máquina de tatuagem (elétricas ou pens) surgem pra facilitar o processo, e demarcam outras expressões culturais. <br />
       <br />
-      hoje, tanto a tattoo com máquina como a sem podem ter sentidos diferentes para cada tatuadore que você conheça, por isso é importante que você pesquise bem sobre  tatuadores que interessam e se você se sente confortável com esse processo. <br />
+      Hoje, tanto a tattoo com máquina como a sem podem ter sentidos diferentes para cada tatuadore que você conheça, por isso é importante que você pesquise bem sobre  tatuadores que interessam e se você se sente confortável com esse processo. <br />
       <br />
 
-      uma sessão comigo tem no mínimo 2h, para além da lentidão do processo de pigmentação, o processo de preparação do espaço (uma parte feita antes do cliente chegar e outra enquanto ele espera) e a aplicação do decalque para que o desenho e o local sejam ideais e adequados ao desejo do cliente são etapas que não podem ser apressadas. <br />
+      Uma sessão comigo tem no mínimo 2h, para além da lentidão do processo de pigmentação, o processo de preparação do espaço (uma parte feita antes do cliente chegar e outra enquanto ele espera) e a aplicação do decalque para que o desenho e o local sejam ideais e adequados ao desejo do cliente são etapas que não podem ser apressadas. <br />
       <br />
-      outras informações importantes são apresentadas depois da marcação e antes da sessão.
+      Outras informações importantes são apresentadas depois da marcação e antes da sessão.
     </p>
   )
 
